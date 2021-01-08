e1cc4a1b85bf8110ccd3465971bbe77eb8658c83
Gnanesh <gnaneshkunal@outlook.com>
2021-01-08 23:14:11 +0530

    Add db practice - part 3

--- src/db.h
+++ src/db.h
@@ -10,5 +10,65 @@ typedef struct {
 } InputBuffer;
 
 InputBuffer *new_input_buffer();
+void read_input(InputBuffer *input_buffer);
+void close_input_buffer(InputBuffer *input_buffer);
+
+typedef enum {
+  EXECUTE_SUCCESS,
+  EXECUTE_TABLE_FULL,
+} ExecuteResult;
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_SYNTAX_ERROR,
+  PREPARE_UNRECOGNIZED_STATEMENT,
+} PrepareResult;
+
+typedef enum {
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+} StatementType;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+typedef struct {
+  StatementType type;
+  Row row_to_insert;            /* only used by insert statement */
+} Statement;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+#define TABLE_MAX_PAGES 100
+
+typedef struct {
+  uint32_t num_rows;
+  void *pages[TABLE_MAX_PAGES];
+} Table;
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table);
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+ExecuteResult execute_statement(Statement *statement, Table *table);
+ExecuteResult execute_select(Statement *statement, Table *table);
+ExecuteResult execute_insert(Statement *statement, Table *table);
+
+void print_row(Row *row);
+void serialize_row(Row *source, void *destination);
+void deserialize_row(void *source, Row *destination);
+void *row_slot(Table *table, uint32_t row_num);
+
+Table *new_table();
+void free_table(Table *table);
 
 #endif
--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
 
 static void print_prompt() { printf("db > "); };
 
