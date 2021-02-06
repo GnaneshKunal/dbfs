96bdb3859f7f27c20e432070baaf452a2d208fe5
Gnanesh <gnaneshkunal@outlook.com>
2021-02-07 01:00:22 +0530

    Splitting a leaf node

--- src/db.h
+++ src/db.h
@@ -102,12 +102,28 @@ void cursor_advance(Cursor *cursor);
 
 typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
 
+void create_new_root(Table *table, uint32_t right_child_page_num);
+
+uint32_t *internal_node_num_keys(void *node);
+uint32_t *internal_node_right_child(void *node);
+uint32_t *internal_node_cell(void *node, uint32_t cell_num);
+uint32_t *internal_node_child(void *node, uint32_t child_num);
+uint32_t *internal_node_key(void *node, uint32_t key_num);
+uint32_t get_node_max_key(void *node);
+
+void initialize_leaf_node(void *node);
+void initialize_internal_node(void *node);
+
 void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value);
+void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value);
 Cursor *leaf_node_find(Table *table, uint32_t page_num, uint32_t key);
 NodeType get_node_type(void *node);
+uint32_t get_unused_page_num(Pager *pager);
+uint32_t get_node_max_key(void *key);
+bool is_node_root(void *node);
 void set_node_type(void *node, NodeType type);
 void print_constants(void);
-void print_leaf_node(void *node);
+void print_tree(Pager *pager, uint32_t page_num, uint32_t identation_level);
 
 #endif
 
